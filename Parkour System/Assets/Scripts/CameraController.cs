2f027dfc8080b6349390e892b4ac91af495502a5
bugbit <ohernandezba@gmail.com>
2024-11-04 22:57:49 +0100

    Configuración de cámara parte

--- Parkour System/Assets/Scripts/CameraController.cs
+++ Parkour System/Assets/Scripts/CameraController.cs
@@ -8,12 +8,23 @@ public class CameraController : MonoBehaviour
     [SerializeField] private GatherInput gInput;
     [SerializeField] private Transform target;
     [SerializeField] private float distance = 5f;
+    [SerializeField] private float minYAangle = -45f;
+    [SerializeField] private float maxYAngle = 45f;
+    [SerializeField] private Vector2 offset;
 
-    private float rotationY;
+    private float rotationY, rotationX;
 
     private void Update()
     {
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        var focusPosition = target.position + new Vector3(offset.x, offset.y);
+
+        rotationX = gInput.mouseInput.y;
         rotationY = gInput.mouseInput.x;
-        transform.position = target.position - Quaternion.Euler(0, rotationY, 0) * new Vector3(0, 0, distance);
+
+        rotationX = Mathf.Clamp(rotationX, minYAangle, maxYAngle);
+
+        transform.position = focusPosition - targetRotation * new Vector3(0, 0, distance);
+        transform.rotation = targetRotation;
     }
 }
