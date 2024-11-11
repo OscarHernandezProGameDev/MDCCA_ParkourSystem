a122f8509c9cfb22cdd084b52c7f8f314baa4b68
Oscar Devs <ohernandezba@gmail.com>
2024-11-11 08:55:14 +0100

    Primera acción de parkour: Método común de acciones

--- Parkour System/Assets/Scripts/ParkourController.cs
+++ Parkour System/Assets/Scripts/ParkourController.cs
@@ -4,27 +4,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(EnvironmentScanner))]
+[RequireComponent(typeof(Animator))]
 public class ParkourController : MonoBehaviour
 {
     [SerializeField] private GatherInput gatherInput;
 
     private EnvironmentScanner scanner;
+    private Animator animator;
+
+    private bool inAction;
 
     private void Awake()
     {
         scanner = GetComponent<EnvironmentScanner>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (gatherInput.tryToJump)
+        if (gatherInput.tryToJump && !inAction)
         {
             var data = scanner.ObstacleCkech();
 
             if (data.forwardHitFound)
-                Debug.Log($"Obstacle found ''{data.forwardHit.transform.name}'' at {data.forwardHit.point}");
+            {
+                StartCoroutine(DoParkourAction());
+            }
 
             gatherInput.tryToJump = false;
         }
+    }
+
+    private IEnumerator DoParkourAction()
+    {
+        inAction = true;
+
+        // No hacemos un play porque queremos hacer una transicion de la animacion actual y la de stepUp
+        animator.CrossFade("StepUp", 0.2f);
+
+        // no se ejecutar hasta llegar a fin del frame
+        yield return null;
+
+        // vamos a obtener la duracion de la animacion stepUp. Para ello usaremos GetNextAnimatorStateInfo y no
+        // GetCurrentAnimatorStateInfo porque esta la transcion de la animacion actual y la de stepUp
+
+        var animState = animator.GetNextAnimatorStateInfo(0);
+
+        Debug.Log($"StepUp Duracion: {animState.length}");
+
+        yield return new WaitForSeconds(animState.length);
+
+        inAction = false;
     }
 }
