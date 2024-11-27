beb4c0e721d26d33d675be2c3b788167d8b04e57
Oscar Devs <ohernandezba@gmail.com>
2024-11-27 22:50:53 +0100

    Conectando puntos de escalada: Climb Points

--- Parkour System/Assets/Scripts/ClimbingSystem/ClimbPoint.cs
+++ Parkour System/Assets/Scripts/ClimbingSystem/ClimbPoint.cs
@@ -1,11 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ClimbPoint : MonoBehaviour
 {
     [SerializeField] private List<Neighbour> neighbours;
+
+    private void Awake()
+    {
+        var twoWayNeighbours = neighbours.Where(n => n.isToWay);
+
+        foreach (var neighbour in twoWayNeighbours)
+        {
+            neighbour.point.CreateConnection(this, -neighbour.direction, neighbour.type, neighbour.isToWay);
+        }
+    }
+
+    public void CreateConnection(ClimbPoint point, Vector2 direction, ConnectionType type, bool isToWay = true)
+    {
+        var neighbour = new Neighbour
+        {
+            point = point,
+            direction = direction,
+            type = type,
+            isToWay = isToWay
+        };
+
+        neighbours.Add(neighbour);
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (var neighbour in neighbours)
+        {
+            if (neighbour.point != null)
+                Debug.DrawLine(transform.position, neighbour.point.transform.position, neighbour.isToWay ? Color.blue : Color.gray);
+        }
+    }
 }
 
 [Serializable]
